0de6b4ae6427520aa653390b628f8fa2857e4cd0
Najmul H. Bappy <http.bappy@gmail.com>
2021-08-12 21:37:03 +0600

    Added question number one

---
+++ quesOne.py
@@ -0,0 +1,12 @@
+
+
+lst = []
+
+# for adding numbers of list use space between numbers. Input: 2 4 6 7 8 9
+lst = [int(item) for item in input("Enter the list of numbers : ").split()]
+
+for l in lst:
+    if l > 150:
+        break
+    if l % 5 == 0:
+        print(l)
