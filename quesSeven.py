5ca322e6a89d9c69841ed14a08f12be1911f9534
Najmul H. Bappy <http.bappy@gmail.com>
2021-08-12 22:39:33 +0600

    added question seven

---
+++ quesSeven.py
@@ -0,0 +1,9 @@
+
+lst = []
+
+lst.append(int(input("Enter the number 1: ")))
+lst.append(int(input("Enter the number 2: ")))
+lst.append(int(input("Enter the number 3: ")))
+
+print("Max Number: ", max(lst))
+print("Min Number: ", min(lst))
